46fb854d1443d228ba738ddc986cca795d7e9efd
YJyamashita <yuuji.yamashita.yg@east.ntt.co.jp>
2022-05-11 22:20:38 +0900

    27

---
+++ src/components/task/TaskCards.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { AddTaskCardButton } from './button/AddTaskCardButton';
+import { TaskCard } from './TaskCard';
+
+export const TaskCards = () =>
+{
+	return (
+		<div>
+			<TaskCard />
+			<AddTaskCardButton />
+		</div>
+	)
+};
