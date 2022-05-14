cccda790823b3ecb57a5ad31c5a2683e8e2b773e
YJyamashita <yuuji.yamashita.yg@east.ntt.co.jp>
2022-05-14 20:08:37 +0900

    39

--- src/components/task/input/TaskAddInput.jsx
+++ src/components/task/input/TaskAddInput.jsx
@@ -1,44 +1,44 @@
-import React from 'react'
+import React from "react";
+import { v4 as uuid } from "uuid";
 
 export const TaskAddInput = ({
-	inputText, setInputText, setTaskList, taskList,
-}) =>
-{
-	const handleSubmit = (e) =>
-	{
-		e.preventDefault();
-		if (inputText === "")
-		{
-			return;
-		}
+  inputText,
+  setInputText,
+  setTaskList,
+  taskList,
+}) => {
+  const handleSubmit = (e) => {
+    const taskId = uuid();
+    e.preventDefault();
+    if (inputText === "") {
+      return;
+    }
+    //カードを追加する。
+    setTaskList([
+      ...taskList,
+      {
+        id: taskId,
+        draggableId: `task-${taskId}`,
+        text: inputText,
+      },
+    ]);
+    setInputText("");
+  };
 
-		// カードを追加する
-		setTaskList([
-			...taskList,
-			{
-				id: taskList.length,
-				draggableId: `task-${taskList.length}`,
-				text: inputText,
-			},
-		]);
-		setInputText("");
-	};
-
-	const handleChange = (e) =>
-	{
-		setInputText(e.target.value);
-		console.log(inputText);
-	};
-
-	return (
-		<div>
-			<form onSubmit={handleSubmit}>
-				<input
-					type="text" placeholder='add a task'
-					className='taskAddInput' onChange={handleChange}
-					value={inputText}
-				/>
-			</form>
-		</div>
-	);
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="add a task"
+          className="taskAddInput"
+          onChange={handleChange}
+          value={inputText}
+        />
+      </form>
+    </div>
+  );
 };
